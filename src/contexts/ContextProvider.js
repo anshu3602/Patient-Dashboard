a020a4a26e869851196a62938dcfbb1bafe6e4c5
Anshu Sandur <112396611+anshu3602@users.noreply.github.com>
2022-08-29 19:58:15 +0530

    Create ContextProvider.js

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const initialState = {
+   userProfile: false,
+    notification: false,
+  };
+  export const ContextProvider = ({ children }) => {
+
+
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState) 
+    
+    
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+    
+    return (
+       
+        <StateContext.Provider value={{
+            activeMenu,setActiveMenu,
+            isClicked, setIsClicked,
+            screenSize, setScreenSize,
+            handleClick,initialState,
+        
+        }}>
+          {children}
+        </StateContext.Provider>
+      );
+    
+};
+
+
+export const useStateContext = () => useContext(StateContext);
